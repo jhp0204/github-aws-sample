0129f62236b1d60a99e328f0b3e0545e5459eb2f
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-08-30 22:43:46 +0900

    Create  provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
