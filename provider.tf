39f5946ad5ac13da9cf693f72b5e99a09e79e488
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-09-10 20:32:13 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-east-2"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+#  access_key = var.AWS_ACCESS_KEY_ID
+#  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
